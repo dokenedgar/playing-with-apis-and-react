2be979eb3284e31c79fdab73c1c85aa19953cc91
McEdgar <dokenedgarjr@gmail.com>
2019-06-26 15:40:40 +0100

    Add home page contents, an intro paragraph as well as an advice of the day obtained from an api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component} from 'react';
 import NavBar from './components/Navbar';
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return(
       <div>
         <NavBar />
+        <Home />
       </div>
     )
   }
