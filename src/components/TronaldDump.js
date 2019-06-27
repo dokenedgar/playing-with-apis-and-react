c449f0ef23d7196c0b2b12d519d437e716ee87f0
McEdgar <dokenedgarjr@gmail.com>
2019-06-27 21:06:32 +0100

    Get the available tags for the topics from the tronaldump api site. Had to use a CORS proxy to by pass the CORS requirements on the API site

--- src/components/TronaldDump.js
+++ src/components/TronaldDump.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+//import Tronald from 'tronalddump-io';
+import { Link } from 'react-router-dom';
 
 class TronaldDump extends Component {
+    state = {
+        tags: []
+    }
+    
+    componentDidMount() {
+        fetch('https://cors-anywhere.herokuapp.com/https://api.tronalddump.io/tag')
+        .then(res => res.json())
+        .then(json => {this.setState({
+            tags:json._embedded
+        })}) 
+        .catch((e)=> console.log(e))
+    }
+    //https://api.tronalddump.io/search/quote?query=
 
     render() {
-        return ('Hey Donald ;)')
+        let tagsLinks = this.state.tags.map(tag => <li><Link to={'/tronald/'+tag}>{tag}</Link></li>)
+        return (
+            <div className="container">
+                <h3 className=" center indigo-text text-darken-3">Donald Trump...</h3>
+                <p className="flow-text">Love him or hate him, but can't ignore him. Anyways, click on any of the links below and see what Mr. President thinks, or said about the person or topic...
+                    Don't read this while eating, you might choke with laughter :)
+                </p>
+                <p className="flow-text"><ul>{tagsLinks} </ul></p>
+            </div>
+        )
     }
 }
 
