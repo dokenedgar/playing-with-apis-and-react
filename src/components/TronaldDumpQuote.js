29390b456524a224421dbb1db5481ebcf63ee142
McEdgar <dokenedgarjr@gmail.com>
2019-06-28 02:24:43 +0100

    Implements next and previous button navigation on quotes about a particular subject

--- src/components/TronaldDumpQuote.js
+++ src/components/TronaldDumpQuote.js
@@ -5,16 +5,23 @@ class TronaldDumpQuote extends Component {
         tag: this.props.match.params.tag,
         receivedQuotes: [],
         next: null,
+        previous: null,
+        current: null,
+        last: null,
+        first: null,
     }
 
     componentDidMount() {
         fetch('https://cors-anywhere.herokuapp.com/https://api.tronalddump.io/search/quote?query='+this.state.tag)
         .then(res => res.json())
         .then(json => {
-           // console.log(json._embedded.quotes)
-            //console.log(json,'\nCurrent items count', json.count, 'Total Items ', json.total)
+            console.log(json)
             this.setState({
                 receivedQuotes: json._embedded.quotes,
+                previous: json._links.prev.href,
+                current: json._links.self.href,
+                last: json._links.last.href,
+                first: json._links.first.href
             })
             if (json._links.next) {
                 this.setState({
@@ -26,15 +33,21 @@ class TronaldDumpQuote extends Component {
     }
 
     componentDidUpdate(){
+        console.log('did update')
         window.scrollTo(0,0)
     }
 
-    nextQuote = () => {
-        fetch('https://cors-anywhere.herokuapp.com/https://api.tronalddump.io'+this.state.next)
+    getQuote = (typeOfQuote) => {
+        fetch('https://cors-anywhere.herokuapp.com/https://api.tronalddump.io'+typeOfQuote)
         .then(res => res.json())
         .then( json => {
+            console.log(json)
             this.setState({
-                receivedQuotes: json._embedded.quotes
+                receivedQuotes: json._embedded.quotes,
+                previous: json._links.prev.href,
+                current: json._links.self.href,
+                last: json._links.last.href,
+                first: json._links.first.href
             })
             if (json._links.next) {
                 this.setState({
@@ -42,13 +55,13 @@ class TronaldDumpQuote extends Component {
                 })
             } else {
                 this.setState({
-                    next: null
+                   // next: null
                 })
             }
         })
     }
     render() {
-       console.log('null',this.state.next)
+      // console.log('null',this.state.next)
         let quotesCard = this.state.receivedQuotes.map((quote,index) => {
             return(
                 <div className="card" key={index}>
@@ -62,23 +75,43 @@ class TronaldDumpQuote extends Component {
                 </div>
             )
         })
-        if(this.state.next !== null) {
+        if(this.state.next === null) {
             return(
                 <div className="center">
                 <h3 className="indigo-text text-darken-4">Trump On {this.state.tag}</h3>
                 {quotesCard}
-                <button className="btn" onClick={this.nextQuote}>next</button>
                 </div>
             )
         }
-        else {
+        if(this.state.first === this.state.current) {
             return(
                 <div className="center">
                 <h3 className="indigo-text text-darken-4">Trump On {this.state.tag}</h3>
                 {quotesCard}
+                <button className="btn" onClick={()=>{this.getQuote(this.state.next)}}>next</button>      
                 </div>
             )
-        } 
+        }
+        if(this.state.last === this.state.current ) {
+            return(
+                <div className="center">
+                <h3 className="indigo-text text-darken-4">Trump On {this.state.tag}</h3>
+                {quotesCard}
+                <button className="btn" onClick={()=>{this.getQuote(this.state.previous)}}>prev</button> &nbsp;      
+                </div>
+            )
+        }
+        if(this.state.current !== this.state.first && this.state.current !== this.state.last) {
+            return(
+                <div className="center">
+                <h3 className="indigo-text text-darken-4">Trump On {this.state.tag}</h3>
+                {quotesCard}
+                <button className="btn" onClick={()=>{this.getQuote(this.state.previous)}}>prev</button> &nbsp; 
+                <button className="btn" onClick={()=>{this.getQuote(this.state.next)}}>next</button>           
+                </div>
+            )
+        }
+
     }
 }
 
